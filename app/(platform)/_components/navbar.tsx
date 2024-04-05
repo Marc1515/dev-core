943b7bc48dbd8767be4ee9b68f56241b7808c1b1
Marc1515 <meq.1515@gmail.com>
2024-04-05 20:26:30 +1100

    The way of the structure I have made is much better to map the other subLabels with them routes

--- app/(platform)/_components/navbar.tsx
+++ app/(platform)/_components/navbar.tsx
@@ -14,6 +14,7 @@ import {
   Building2,
   Database,
 } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const routes = [
   {
@@ -23,21 +24,41 @@ const routes = [
   },
   {
     label: "Fundamentals",
+    subLabels: [
+      { subLabel: "label 1", href: "/label1" },
+      { subLabel: "label 2", href: "/label2" },
+      { subLabel: "label 3", href: "/label3" },
+    ],
     icon: <BrickWall className="w-4 h-4" />,
     href: "/fundamentals",
   },
   {
     label: "Front-End",
+    subLabels: [
+      { subLabel: "label 1", href: "/label1" },
+      { subLabel: "label 2", href: "/label2" },
+      { subLabel: "label 3", href: "/label3" },
+    ],
     icon: <MonitorSmartphone className="w-4 h-4" />,
     href: "/frontend",
   },
   {
     label: "Back-End",
+    subLabels: [
+      { subLabel: "label 1", href: "/label1" },
+      { subLabel: "label 2", href: "/label2" },
+      { subLabel: "label 3", href: "/label3" },
+    ],
     icon: <Building2 className="w-4 h-4" />,
     href: "/backend",
   },
   {
     label: "Data-Base",
+    subLabels: [
+      { subLabel: "label 1", href: "/label1" },
+      { subLabel: "label 2", href: "/label2" },
+      { subLabel: "label 3", href: "/label3" },
+    ],
     icon: <Database className="w-4 h-4" />,
     href: "/database",
   },
@@ -55,7 +76,12 @@ export const Navbar = () => {
                   <span className="font-medium text-sm">{route.label}</span>
                 </div>
               </AccordionTrigger>
-              <AccordionContent>Content</AccordionContent>
+              <AccordionContent>
+                {route.subLabels &&
+                  route.subLabels.map((subLabel) => (
+                    <Button key={subLabel.href}>{subLabel.subLabel}</Button>
+                  ))}
+              </AccordionContent>
             </AccordionItem>
           </Accordion>
         ))}
