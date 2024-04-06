f052e5ebf3558d5a1e7b2360cdb64716f4a9db6b
Marc1515 <meq.1515@gmail.com>
2024-04-06 14:21:30 +1100

    NavbarData importation and all the modification to set it

--- app/(platform)/_components/navbar.tsx
+++ app/(platform)/_components/navbar.tsx
@@ -1,91 +1,81 @@
+"use client";
+
 import React from "react";
-import { NavItem } from "./nav-item";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
 
 import {
-  Home,
-  BrickWall,
-  MonitorSmartphone,
-  Building2,
-  Database,
-} from "lucide-react";
-import { Button } from "@/components/ui/button";
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
-const routes = [
-  {
-    label: "Home",
-    icon: <Home className="w-4 h-4" />,
-    href: "/",
-  },
-  {
-    label: "Fundamentals",
-    subLabels: [
-      { subLabel: "label 1", href: "/label1" },
-      { subLabel: "label 2", href: "/label2" },
-      { subLabel: "label 3", href: "/label3" },
-    ],
-    icon: <BrickWall className="w-4 h-4" />,
-    href: "/fundamentals",
-  },
-  {
-    label: "Front-End",
-    subLabels: [
-      { subLabel: "label 1", href: "/label1" },
-      { subLabel: "label 2", href: "/label2" },
-      { subLabel: "label 3", href: "/label3" },
-    ],
-    icon: <MonitorSmartphone className="w-4 h-4" />,
-    href: "/frontend",
-  },
-  {
-    label: "Back-End",
-    subLabels: [
-      { subLabel: "label 1", href: "/label1" },
-      { subLabel: "label 2", href: "/label2" },
-      { subLabel: "label 3", href: "/label3" },
-    ],
-    icon: <Building2 className="w-4 h-4" />,
-    href: "/backend",
-  },
-  {
-    label: "Data-Base",
-    subLabels: [
-      { subLabel: "label 1", href: "/label1" },
-      { subLabel: "label 2", href: "/label2" },
-      { subLabel: "label 3", href: "/label3" },
-    ],
-    icon: <Database className="w-4 h-4" />,
-    href: "/database",
-  },
-];
+import { navbarData } from "@/constants/navbarData";
 
 export const Navbar = () => {
   return (
-    <nav className="w-full h-14 top-0 border-b border-white ">
-      <div className="flex justify-center gap-x-3">
-        {routes.map((route) => (
-          <Accordion key={route.label} type="multiple">
-            <AccordionItem value={route.label} className="border-none">
-              <AccordionTrigger className="flex">
-                <div className="flex items-center gap-x-2">
-                  <span className="font-medium text-sm">{route.label}</span>
-                </div>
-              </AccordionTrigger>
-              <AccordionContent>
-                {route.subLabels &&
-                  route.subLabels.map((subLabel) => (
-                    <Button key={subLabel.href}>{subLabel.subLabel}</Button>
-                  ))}
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        ))}
-      </div>
-    </nav>
+    <div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          {navbarData.map((component) => (
+            <NavigationMenuItem key={component.title}>
+              <NavigationMenuTrigger>{component.title}</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                  {component.items &&
+                    component.items.map((component) => (
+                      <ListItem
+                        key={component.label}
+                        title={component.label}
+                        href={component.href}
+                      >
+                        {component.description}
+                      </ListItem>
+                    ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
   );
 };
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
