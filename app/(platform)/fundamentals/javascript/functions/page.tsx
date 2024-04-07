09fa4aea8ac7efe53b0bce57c8e4b3b24a3f7db4
Marc1515 <meq.1515@gmail.com>
2024-04-07 14:21:36 +1000

    New path to explain the functions deeply

---
+++ app/(platform)/fundamentals/javascript/functions/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FunctionsPage = () => {
+  return (
+    <div>
+      <h1>Functions</h1>
+      <p>
+        Functions in JavaScript are blocks of code designed to perform a
+        specific task. They are defined once and can be invoked or {"called"} as
+        many times as necessary, which allows for code reuse and more efficient
+        organization. Functions can receive data as input, called parameters,
+        process that data, and return a result.
+      </p>
+    </div>
+  );
+};
+
+export default FunctionsPage;
