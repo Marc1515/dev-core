e3363fa540c1f1ac8d4d34d7e2317cb1ee554c79
Marc1515 <meq.1515@gmail.com>
2024-04-07 13:30:01 +1000

    I've created a components section to JavaScript section, that way the code will be better to maintain

---
+++ app/(platform)/fundamentals/javascript/_components/ObjectTypes.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const ObjectTypes = () => {
+  return (
+    <div>
+      <span>Objetc Types</span>
+      <p>
+        JavaScript allows you to create objects, which are collections of
+        properties:
+      </p>
+      <ul>
+        <li>
+          <p>
+            <strong>Object:</strong>The most basic structure for storing data in
+            a key-value format. For example, {"{ name: 'John', age: 30 }"}.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Array:</strong>A special kind of object used for storing
+            ordered collections. For example, {"['apple', 'banana', 'cherry']"}.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Function:</strong>A block of code designed to perform a
+            particular task. Functions are objects too.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+};
---
+++ app/(platform)/fundamentals/javascript/_components/PrimitiveTypes.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+export const PrimitiveTypes = () => {
+  return (
+    <div>
+      <span>Primitive Types</span>
+      <p>These are the basics types of data:</p>
+      <ul>
+        <li>
+          <p>
+            <strong>String:</strong>It&apos;s a sequence of characters used to
+            represent text. For example, &apos;Hello, World!&apos;.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Number:</strong>Represents both integer and floating-point
+            numbers. For example, 42 or 3.14. JavaScript uses double-precision
+            floating-point format for numbers.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>BigInt:</strong>An integer with arbitrary precision.
+            It&apos;s useful for very large numbers. For example,
+            9007199254740991n.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Boolean:</strong>Represents a logical entity and can have
+            two values: true and false.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Undefined:</strong>A variable that has not been assigned a
+            value has the value undefined.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Null:</strong>Denotes a null value. It&apos;s used to
+            represent a non-existent or invalid object or address.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Symbol:</strong>A unique and immutable primitive introduced
+            in ES6, used as the key of an Object property.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+};
