6d0174fb55d8deb26d3a86b705a831322e64d17d
Marc1515 <meq.1515@gmail.com>
2024-04-08 11:29:53 +1000

    Just the components importations

--- app/(platform)/fundamentals/javascript/functions/page.tsx
+++ app/(platform)/fundamentals/javascript/functions/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { FunctionsDeclarations } from "./_components/FunctionsDeclarations";
+import FunctionsExpressions from "./_components/FunctionsExpression";
+import ArrowFunctions from "./_components/ArrowFunctions";
+import ConstructorFunctions from "./_components/ConstructorFunctions";
+import GeneratorFunctions from "./_components/GeneratorFunctions";
+import AsyncFunctions from "./_components/AsyncFunctions";
+import Closures from "./_components/Closures";
+import Callbacks from "./_components/Callbacks";
 
 const FunctionsPage = () => {
   return (
@@ -13,6 +20,19 @@ const FunctionsPage = () => {
         process that data, and return a result.
       </p>
       <FunctionsDeclarations />
+      <FunctionsExpressions />
+      <ArrowFunctions />
+      <ConstructorFunctions />
+      <GeneratorFunctions />
+      <AsyncFunctions />
+      <Closures />
+      <Callbacks />
+      <p>
+        Each type of function in JavaScript has its purpose and situations where
+        it is most useful. Understanding these different types will allow you to
+        write clearer and more effective code, choosing the form of function
+        that best suits your needs in each case.
+      </p>
     </div>
   );
 };
