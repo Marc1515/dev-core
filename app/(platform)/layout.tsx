521409128c05266312e991c1bfc1874f03991134
Marc1515 <meq.1515@gmail.com>
2024-04-08 10:22:17 +1000

    I need to set the sonner component called Toaster if I want to use it in all my app

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,8 +1,10 @@
+import { Toaster } from "sonner";
 import { Navbar } from "./_components/Navbar";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <Toaster position="top-right" />
       <Navbar />
       <div>{children}</div>
     </>
