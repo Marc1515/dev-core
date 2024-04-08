ef7799b2a68e3509e0115524333f23cbb2fe5a10
Marc1515 <meq.1515@gmail.com>
2024-04-08 10:21:14 +1000

    This component will need more modifications as the others but at this moment the user can copy the code showed as example

---
+++ app/(platform)/fundamentals/javascript/functions/_components/FunctionsDeclarations.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+import ClipboardButton from "@/app/(platform)/_components/ClipboardButton";
+
+export const FunctionsDeclarations = () => {
+  const codeToCopy = `
+  
+    function greet(name) {
+      return \`Hello, \${name}!\`;
+    }
+    console.log(greet('Ana')); // "Hello, Ana!"
+
+    `;
+
+  return (
+    <div>
+      <span>Functions Declarations</span>
+      <p>
+        Function declarations are perhaps the most traditional way of defining
+        functions in JavaScript. They are characterized by having a name and do
+        not require assignment to a variable. A distinctive aspect of this type
+        of function is that they are hoisted, meaning they can be called before
+        they appear in the code due to how JavaScript interprets functions.
+      </p>
+      <div>
+        <pre>{codeToCopy}</pre>
+        <ClipboardButton textToCopy={codeToCopy} />
+      </div>
+    </div>
+  );
+};
