b6f60eed3fa2dd5da164208bb8c1f0f173b80a5e
Marc1515 <meq.1515@gmail.com>
2024-04-09 19:35:02 +1000

    The best way to use the language provider cause I will use it in all the app

--- app/[locale]/(platform)/layout.tsx
+++ app/[locale]/(platform)/layout.tsx
@@ -1,13 +1,32 @@
+import initTranslations from "@/app/i18n";
+import TranslationsProvider from "@/lib/TranslationProvider";
 import { Toaster } from "sonner";
 import { Navbar } from "./_components/Navbar";
 
-const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
+interface PlatformLayoutProps {
+  children: React.ReactNode;
+  params: {
+    locale: string;
+  };
+}
+
+const i18nNamespaces = [""];
+
+const PlatformLayout = async ({ children, params }: PlatformLayoutProps) => {
+  const { locale } = params;
+
+  const { resources } = await initTranslations(locale, i18nNamespaces);
+
   return (
-    <>
+    <TranslationsProvider
+      resources={resources}
+      locale={locale}
+      namespaces={i18nNamespaces}
+    >
       <Toaster position="top-right" />
       <Navbar />
       <div>{children}</div>
-    </>
+    </TranslationsProvider>
   );
 };
 
