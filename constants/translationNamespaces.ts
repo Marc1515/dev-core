a04875f4bcf8f5ea49dc1c9d088331323d4a4c8a
Marc1515 <meq.1515@gmail.com>
2024-04-12 16:35:58 +1000

    New namespases for the operators

--- constants/translationNamespaces.ts
+++ constants/translationNamespaces.ts
@@ -12,4 +12,6 @@ export const primitiveTypesNamespaces = [...commonNamespaces, "primitiveTypes"];
 
 export const functionsNamespaces = [...commonNamespaces, "functions"];
 
+export const operatorsNamespaces = [...commonNamespaces, "operators"];
+
 // Y así sucesivamente para otras partes de la aplicación
