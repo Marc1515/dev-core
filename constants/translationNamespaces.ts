6e4a531f36e83503674976758d305f488cac5593
Marc1515 <meq.1515@gmail.com>
2024-04-12 14:52:54 +1000

    In this file I will centralize all the namespaces from i18nexus

---
+++ constants/translationNamespaces.ts
@@ -0,0 +1,15 @@
+// translationNamespaces.ts
+
+export const commonNamespaces = ["common"];
+
+export const navbarTypesNamespaces = [...commonNamespaces, "navbar"];
+
+export const javascriptTypesNamespaces = [...commonNamespaces, "javascript"];
+
+export const objectTypesNamespaces = [...commonNamespaces, "objectTypes"];
+
+export const primitiveTypesNamespaces = [...commonNamespaces, "primitiveTypes"];
+
+export const functionsNamespaces = [...commonNamespaces, "functions"];
+
+// Y así sucesivamente para otras partes de la aplicación
