2838afa69e19ddc57e31be73ea456d0b0057ea20
Marc1515 <meq.1515@gmail.com>
2024-04-14 21:55:15 +1000

    Just adding the new content with their own next Links

--- app/[locale]/(platform)/fundamentals/javascript/page.tsx
+++ app/[locale]/(platform)/fundamentals/javascript/page.tsx
@@ -36,6 +36,22 @@ const JavaScriptPage = async ({ params: { locale } }: JavaScriptPageProps) => {
         <span>{commonT("control_structures_title")}</span>
       </Link>
       <p>{mainT("control_strucutres_description")}</p>
+      <Link href={"./javascript/functions"}>
+        <span>{commonT("functions_title")}</span>
+      </Link>
+      <p>{mainT("functions_description")}</p>
+      <Link href={"./javascript/methods"}>
+        <span>{commonT("methods_title")}</span>
+      </Link>
+      <p>{mainT("methods_description")}</p>
+      <Link href={"./javascript/stringsmanipulation"}>
+        <span>{commonT("strings_manipulation_title")}</span>
+      </Link>
+      <p>{mainT("strings_manipulation_description")}</p>
+      <Link href={"./javascript/arraysmanipulation"}>
+        <span>{commonT("arrays_manipulation_title")}</span>
+      </Link>
+      <p>{mainT("arrays_manipulation_description")}</p>
     </>
   );
 };
