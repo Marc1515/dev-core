bb97c1c0c1ccca82945e80acb9bc3b576318265a
Marc1515 <meq.1515@gmail.com>
2024-04-18 13:56:06 +1000

    I have just deleted code that I'm not using

--- hooks/useTranslations.tsx
+++ hooks/useTranslations.tsx
@@ -2,7 +2,6 @@
 import { useState, useEffect } from "react";
 import initTranslations from "@/app/i18n";
 import { useLocale } from "@/contexts/LocaleContext";
-import i18next from "i18next";
 
 export function useTranslations(namespaces: string[]) {
   const { locale } = useLocale();
@@ -14,7 +13,6 @@ export function useTranslations(namespaces: string[]) {
     async function loadTranslations() {
       const translationsResult = await initTranslations(locale, namespaces);
       setTranslations(translationsResult);
-      i18next.t("array_donde_quiero_la_variable", { returnObjects: true });
     }
 
     loadTranslations();
