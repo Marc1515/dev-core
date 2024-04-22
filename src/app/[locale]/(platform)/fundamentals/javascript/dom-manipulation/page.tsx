f419ca4517583a2c94a46b02dd9bb99997930631
Marc1515 <meq.1515@gmail.com>
2024-04-22 18:50:17 +1000

    dom modification main page finished

--- src/app/[locale]/(platform)/fundamentals/javascript/dom-manipulation/page.tsx
+++ src/app/[locale]/(platform)/fundamentals/javascript/dom-manipulation/page.tsx
@@ -1,15 +1,8 @@
 import React from "react";
 import initTranslations from "@/app/i18n";
-import {
-  commonNamespaces,
-  domManipulationNamespaces,
-} from "@/constants/translationNamespaces";
-import { SelectDOMElements } from "./_components/SelectDOMElements";
-import { ElementContentModification } from "./_components/ElementContentModification";
-import { StyleChange } from "./_components/StyleChange";
-import { AddOrRemoveElements } from "./_components/AddOrRemoveElements";
-import { EventsHandle } from "./_components/EventsHandle";
-import { DOMNavigation } from "./_components/DOMNavigation";
+import { domManipulationNamespaces } from "@/constants/translationNamespaces";
+import { DomManipulationTypes } from "./types";
+import Link from "next/link";
 
 interface DOMManipulationPageProps {
   params: {
@@ -20,24 +13,27 @@ interface DOMManipulationPageProps {
 const DOMManipulationPage = async ({
   params: { locale },
 }: DOMManipulationPageProps) => {
-  const { t: commonT } = await initTranslations(locale, commonNamespaces);
-  const { t: mainT } = await initTranslations(
-    locale,
-    domManipulationNamespaces
-  );
+  const { t } = await initTranslations(locale, domManipulationNamespaces);
+  const data = t("data", {
+    returnObjects: true,
+  }) as DomManipulationTypes["data"];
 
   return (
     <div>
-      <h1>{commonT("dom_manipulation_title")}</h1>
-      <p>{commonT("dom_manipulation_definition")}</p>
-      <h2>{mainT("domm_key_concepts_title")}</h2>
-      <SelectDOMElements />
-      <h2>{mainT("domm_basic_operations_in_the_dom_title")}</h2>
-      <ElementContentModification />
-      <StyleChange />
-      <AddOrRemoveElements />
-      <EventsHandle />
-      <DOMNavigation />
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+      <ul>
+        {data.items.map((item, idx) => (
+          <li key={idx}>
+            <span>
+              <strong>
+                <Link href={item.path}>{item.title}</Link>
+              </strong>
+            </span>
+            <p>{item.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
