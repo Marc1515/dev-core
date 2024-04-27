00b0844ecea6606285c64a5ce5d7eafff5e681a7
Marc1515 <meq.1515@gmail.com>
2024-04-27 15:56:41 +1000

    I'm using the next icon as a placeholder when I finish my own I will change it

--- src/app/[locale]/(platform)/_components/MobileNavbar/index.tsx
+++ src/app/[locale]/(platform)/_components/MobileNavbar/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+
+import Image from "next/image";
+
 import { BurgerButton } from "./_components/BurgerButton";
 
 export const MobileNavbar = () => {
   return (
-    <div className="fixed top-0 flex justify-end w-full z-20 bg-transparent backdrop-blur-sm">
+    <div className="fixed top-0 pl-5 flex justify-between w-full z-20 bg-transparent backdrop-blur-sm">
+      <Image src="/next.svg" alt="Next Icon" width={100} height={100} />
       <BurgerButton />
     </div>
   );
