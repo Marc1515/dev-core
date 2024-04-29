de08cefd2d75661692b8213ddaa80bbf66305634
Marc1515 <meq.1515@gmail.com>
2024-04-29 21:44:50 +1000

    reusable structure set

--- src/app/[locale]/(platform)/fundamentals/javascript/syntax-and-basic-concepts/controlstructures/_components/ControlStructures/index.tsx
+++ src/app/[locale]/(platform)/fundamentals/javascript/syntax-and-basic-concepts/controlstructures/_components/ControlStructures/index.tsx
@@ -1,60 +1,73 @@
 "use client";
 
-import React from "react";
+import React, { Fragment } from "react";
 import { ControlStructuresTypes } from "./types";
 import { ConditionalCodes, RepetitionCodes } from "./codes";
 import ClipboardButton from "@/app/[locale]/(platform)/_components/ClipboardButton";
+import { BasicBoxWrapper } from "@/app/[locale]/(platform)/_components/BasicBoxWrapper";
+import { IntroTitle } from "@/app/[locale]/(platform)/_components/BasicBoxWrapper/_components/IntroTitle";
+import { BasicDescription } from "@/app/[locale]/(platform)/_components/BasicBoxWrapper/_components/BasicDescription";
+import { SecondaryTitle } from "@/app/[locale]/(platform)/_components/BasicBoxWrapper/_components/SecondaryTitle";
+import { ListWrapper } from "@/app/[locale]/(platform)/_components/ListWrapper";
+import { ContentList } from "@/app/[locale]/(platform)/_components/ListWrapper/_components/ContentList";
+import { ItemList } from "@/app/[locale]/(platform)/_components/ListWrapper/_components/ContentList/_components/ItemsList";
+import { DescriptionItemList } from "@/app/[locale]/(platform)/_components/ListWrapper/_components/ContentList/_components/ItemsList/_components/DescriptionItemList";
+import { CodeBox } from "@/app/[locale]/(platform)/_components/BasicBoxWrapper/_components/CodeBox";
 
 export const ControlStructures = ({ data }: ControlStructuresTypes) => {
   return (
     <div>
       {/* Introduction */}
-      <h1>{data.title}</h1>
-      <p>{data.description}</p>
+      <BasicBoxWrapper>
+        <IntroTitle>{data.title}</IntroTitle>
+        <BasicDescription>{data.description}</BasicDescription>
+      </BasicBoxWrapper>
       {/* Main Function */}
-      <span>{data.main_function.title}</span>
-      <p>{data.main_function.description}</p>
+      <BasicBoxWrapper>
+        <SecondaryTitle>{data.main_function.title}</SecondaryTitle>
+        <BasicDescription>{data.main_function.description}</BasicDescription>
+      </BasicBoxWrapper>
       {/* Importance */}
-      <span>{data.importance.title}</span>
-      <ul>
-        {data.importance.items.map((item, index) => (
-          <li key={index}>
-            <p>{item.description}</p>
-          </li>
-        ))}
-      </ul>
+      <ListWrapper>
+        <SecondaryTitle>{data.importance.title}</SecondaryTitle>
+        <ContentList>
+          {data.importance.items.map((item, index) => (
+            <ItemList key={index}>
+              <DescriptionItemList>{item.description}</DescriptionItemList>
+            </ItemList>
+          ))}
+        </ContentList>
+      </ListWrapper>
       {/* Structures */}
-      {data.structures.map((structure, index) => (
-        <div key={index}>
-          <span>{structure.title}</span>
-          <p>{structure.description}</p>
-          {structure.title === "Repetition Structures (Loops)"
-            ? structure.items?.map((item, idx) => (
-                <div key={idx}>
-                  <p>
-                    <strong>{item.title}: </strong>
-                    {item.description}
-                  </p>
-                  <div>
-                    <pre>{RepetitionCodes[idx]}</pre>
-                    <ClipboardButton textToCopy={RepetitionCodes[idx]} />
-                  </div>
-                </div>
-              ))
-            : structure.items?.map((item, idx) => (
-                <div key={idx}>
-                  <p>
-                    <strong>{item.title}: </strong>
-                    {item.description}
-                  </p>
-                  <div>
-                    <pre>{ConditionalCodes[idx]}</pre>
-                    <ClipboardButton textToCopy={ConditionalCodes[idx]} />
-                  </div>
-                </div>
-              ))}
-        </div>
-      ))}
+      <BasicBoxWrapper>
+        {data.structures.map((structure, index) => (
+          <Fragment key={index}>
+            <SecondaryTitle>{structure.title}</SecondaryTitle>
+            <BasicDescription>{structure.description}</BasicDescription>
+            {structure.title === "Repetition Structures (Loops)"
+              ? structure.items?.map((item, idx) => (
+                  <Fragment key={idx}>
+                    <SecondaryTitle>{item.title}</SecondaryTitle>
+                    <BasicDescription>{item.description}</BasicDescription>
+                    <>
+                      <CodeBox>{RepetitionCodes[idx]}</CodeBox>
+                      <ClipboardButton textToCopy={RepetitionCodes[idx]} />
+                    </>
+                  </Fragment>
+                ))
+              : structure.items?.map((item, idx) => (
+                  <Fragment key={idx}>
+                    <SecondaryTitle>{item.title}</SecondaryTitle>
+                    <BasicDescription>{item.description}</BasicDescription>
+                    <>
+                      <CodeBox>{ConditionalCodes[idx]}</CodeBox>
+                      <ClipboardButton textToCopy={ConditionalCodes[idx]} />
+                    </>
+                  </Fragment>
+                ))}
+          </Fragment>
+        ))}
+      </BasicBoxWrapper>
     </div>
   );
 };
