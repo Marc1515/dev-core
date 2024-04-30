cd223334a27ff80f50fb3134a5aec751805fc8f8
Marc1515 <meq.1515@gmail.com>
2024-04-30 18:43:00 +1000

    I've styled a litle bit the mobileSidebar and set some icons

--- src/app/[locale]/(platform)/_components/MobileSidebar/index.tsx
+++ src/app/[locale]/(platform)/_components/MobileSidebar/index.tsx
@@ -14,6 +14,13 @@ import {
 } from "@/components/ui/accordion";
 import Link from "next/link";
 import { useBurgerButtonContext } from "@/contexts/BurgerButtonContext";
+import { Separator } from "@/components/ui/separator";
+import { BiSolidHome } from "react-icons/bi";
+
+import { FaCubes } from "react-icons/fa";
+import { FiMonitor } from "react-icons/fi";
+import { FaServer } from "react-icons/fa";
+import { BsServer } from "react-icons/bs";
 
 export const MobileSidebar = ({ data }: MobileSidebarTypes) => {
   const moblileSidebarData = getMobileSidebarData(data);
@@ -28,18 +35,32 @@ export const MobileSidebar = ({ data }: MobileSidebarTypes) => {
       } md:hidden`}
     >
       <Link onClick={sheetCloserHandler} href="/">
-        <span>{data.home.title}</span>
+        <div className="flex justify-between items-center">
+          <BiSolidHome className="h-8 text-xl" />
+          <span className="text-xl">{data.home.title}</span>
+          <div className="pr-5"></div>
+        </div>
       </Link>
+      <Separator />
       <Accordion type="single" collapsible>
         {moblileSidebarData.map((item, idx) => (
           <AccordionItem key={idx} value={item.title}>
-            <AccordionTrigger>{item.title}</AccordionTrigger>
+            <AccordionTrigger>
+              <div>
+                {idx === 0 ? <FaCubes className="h-8 text-xl" /> : null}
+                {idx === 1 ? <FiMonitor className="h-8 text-xl" /> : null}
+                {idx === 2 ? <FaServer className="h-8 text-xl" /> : null}
+                {idx === 3 ? <BsServer className="h-8 text-xl" /> : null}
+              </div>
+
+              <p className="text-lg">{item.title}</p>
+            </AccordionTrigger>
             <AccordionContent>
               <ul>
                 {item.items.map((item, idx) => (
                   <li key={idx}>
                     <Link onClick={sheetCloserHandler} href={item.href}>
-                      {item.label}
+                      <p className="text-md py-3">{item.label}</p>
                     </Link>
                   </li>
                 ))}
