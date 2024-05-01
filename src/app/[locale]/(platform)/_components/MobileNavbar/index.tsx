977d339005d64907d222e6294fd25406a6b62cce
Marc1515 <meq.1515@gmail.com>
2024-05-01 19:51:07 +1000

    I have imported the dark mode and set it between the icon and the burger button

--- src/app/[locale]/(platform)/_components/MobileNavbar/index.tsx
+++ src/app/[locale]/(platform)/_components/MobileNavbar/index.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import Image from "next/image";
 
 import { BurgerButton } from "./_components/BurgerButton";
+import { DarkModeButton } from "../DarkModeButton";
 
 export const MobileNavbar = () => {
   return (
     <div className="fixed top-0 pl-5 flex justify-between w-full z-20 bg-transparent backdrop-blur-sm">
       <Image src="/next.svg" alt="Next Icon" width={100} height={100} />
+      <DarkModeButton />
       <BurgerButton />
     </div>
   );
