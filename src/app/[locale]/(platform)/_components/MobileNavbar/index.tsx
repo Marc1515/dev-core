4078bf7df1534530508c90e1e717050d852a324b
Marc1515 <meq.1515@gmail.com>
2024-05-02 21:06:54 +1000

    I have just centred vertically the component

--- src/app/[locale]/(platform)/_components/MobileNavbar/index.tsx
+++ src/app/[locale]/(platform)/_components/MobileNavbar/index.tsx
@@ -7,7 +7,7 @@ import { DarkModeButton } from "../DarkModeButton";
 
 export const MobileNavbar = () => {
   return (
-    <div className="fixed top-0 pl-5 flex justify-between w-full z-20 bg-transparent backdrop-blur-sm">
+    <div className="fixed top-0 pl-5 flex justify-between items-center w-full z-20 bg-transparent backdrop-blur-sm">
       <Image src="/next.svg" alt="Next Icon" width={100} height={100} />
       <DarkModeButton />
       <BurgerButton />
