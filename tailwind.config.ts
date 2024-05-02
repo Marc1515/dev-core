5b1996d7c6468cab30bf12a3110d00c938a364f9
Marc1515 <meq.1515@gmail.com>
2024-05-02 16:08:50 +1000

    I'm applaying some custom styles for the app's background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,7 +22,6 @@ const config: Config = {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
@@ -72,8 +71,8 @@ const config: Config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-      backgroundColor: {
-        "custom-background": "var(--background)",
+      backgroundImage: {
+        "custom-gradient": "var(--custom-gradient)",
       },
     },
   },
