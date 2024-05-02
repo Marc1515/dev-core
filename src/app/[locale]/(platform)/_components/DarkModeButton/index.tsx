9f1d55e577c95fe7278029fbbfbb3cdbead35537
Marc1515 <meq.1515@gmail.com>
2024-05-02 16:11:36 +1000

    I've set with and useEffect that if I change the state is has to change the theme stored at the localstorage

--- src/app/[locale]/(platform)/_components/DarkModeButton/index.tsx
+++ src/app/[locale]/(platform)/_components/DarkModeButton/index.tsx
@@ -1,18 +1,35 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import React, { useState } from "react";
-import { useDarkModeContext } from "@/contexts/DarkModeContext";
 import { MdOutlineLightMode } from "react-icons/md";
 import { MdDarkMode } from "react-icons/md";
 
 export const DarkModeButton = () => {
-  const { isDarkActive, toggleDarkMode } = useDarkModeContext();
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") setDarkMode(true);
+  }, []);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
 
   return (
     <>
-      <Button onClick={toggleDarkMode}>
-        {!isDarkActive ? <MdOutlineLightMode /> : <MdDarkMode />}
+      <Button
+        onClick={() => setDarkMode(!darkMode)}
+        className="text-foreground dark:text-foreground dark:bg-custom-gradient bg-custom-gradient"
+      >
+        {!darkMode ? <MdOutlineLightMode /> : <MdDarkMode />}
       </Button>
     </>
   );
