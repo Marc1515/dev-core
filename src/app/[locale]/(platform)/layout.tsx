bb5a4d73ed29d436c0ee3917d0f6857dcfd13c30
Marc1515 <meq.1515@gmail.com>
2024-05-02 16:17:31 +1000

    I'm not using the provider more so I don't have to import it

--- src/app/[locale]/(platform)/layout.tsx
+++ src/app/[locale]/(platform)/layout.tsx
@@ -7,7 +7,6 @@ import { MobileSidebar } from "./_components/MobileSidebar";
 import { BurgerButtonProvider } from "@/contexts/BurgerButtonContext";
 import { MainWrapperContent } from "./_components/Wrappers";
 import { MobileNavbar } from "./_components/MobileNavbar";
-import { DarkModeProvider } from "@/contexts/DarkModeContext";
 
 interface PlatformLayoutProps {
   children: React.ReactNode;
@@ -32,15 +31,13 @@ const PlatformLayout = async ({ children, params }: PlatformLayoutProps) => {
       locale={locale}
       namespaces={i18nNamespaces}
     >
-      <DarkModeProvider>
-        <Toaster position="top-right" />
-        <Navbar data={data} />
-        <BurgerButtonProvider>
-          <MobileNavbar />
-          <MobileSidebar data={data} />
-        </BurgerButtonProvider>
-        <MainWrapperContent>{children}</MainWrapperContent>
-      </DarkModeProvider>
+      <Toaster position="top-right" />
+      <Navbar data={data} />
+      <BurgerButtonProvider>
+        <MobileNavbar />
+        <MobileSidebar data={data} />
+      </BurgerButtonProvider>
+      <MainWrapperContent>{children}</MainWrapperContent>
     </TranslationsProvider>
   );
 };
