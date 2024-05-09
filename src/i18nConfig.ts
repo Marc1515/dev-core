e4b03e559c75bdfee2abcd954077f482ba271521
Marc España Quinquilla <meq.1515@gmail.com>
2024-05-09 15:58:50 +0200

    It was need to add the interface if I want to convert it to a TypeScript

--- src/i18nConfig.ts
+++ src/i18nConfig.ts
@@ -1,6 +1,13 @@
-const i18nConfig = {
+interface I18nConfig {
+  locales: string[];
+  defaultLocale: string;
+  prefixDefault?: boolean;
+}
+
+const i18nConfig: I18nConfig = {
   locales: ["en", "es"],
   defaultLocale: "en",
+  prefixDefault: false,
 };
 
 export default i18nConfig;
