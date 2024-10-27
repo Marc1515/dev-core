6226477cb70c804cc3eff345c667648cbb1e356b
Marc España Quinquilla <meq.1515@gmail.com>
2024-10-27 12:15:46 +0100

    After thing about how I´m managing the server components I will do some changes to do it more simple

--- src/app/[locale]/(platform)/fundamentals/javascript/syntax-and-basic-concepts/variables/page.tsx
+++ src/app/[locale]/(platform)/fundamentals/javascript/syntax-and-basic-concepts/variables/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
 import initTranslations from "@/app/i18n";
 import { variablesNamespaces } from "@/constants/namespaces/javaScriptNamespaces";
-import { Variables } from "./_components/Variables";
+import { VariablesTypes } from "@/types/variables";
+/* Wrappers */
+import {
+  BasicBoxWrapper,
+  ListWrapper,
+} from "@/app/[locale]/(platform)/_components/Wrappers";
+/* Titles */
+import {
+  IntroTitle,
+  SecondaryTitle,
+} from "@/app/[locale]/(platform)/_components/Titles";
+/* Paragraphs */
+import {
+  BasicDescription,
+  Conclusion,
+} from "@/app/[locale]/(platform)/_components/Paragraphs";
+/* Lists */
+import {
+  List,
+  ItemList,
+  ListTitle,
+  ItemListTitle,
+  ItemListDescription,
+} from "@/app/[locale]/(platform)/_components/Lists";
 
 interface VariablesProps {
   params: {
@@ -11,12 +33,41 @@ interface VariablesProps {
 
 const VariablesPage = async ({ params: { locale } }: VariablesProps) => {
   const { t } = await initTranslations(locale, variablesNamespaces);
-  const data = t("data", { returnObjects: true });
+  const data: VariablesTypes["data"] = t("data", { returnObjects: true });
 
   return (
-    <div>
-      <Variables data={data} />
-    </div>
+    <>
+      {/* Variables */}
+      <>
+        <IntroTitle>{data.title}</IntroTitle>
+        <BasicDescription>{data.description}</BasicDescription>
+      </>
+
+      {/* Main Function */}
+      <BasicBoxWrapper>
+        <SecondaryTitle>{data.main_function.title}</SecondaryTitle>
+        <BasicDescription>{data.main_function.description}</BasicDescription>
+      </BasicBoxWrapper>
+
+      {/* Importance */}
+      <BasicBoxWrapper>
+        <ListTitle>{data.importance.title}</ListTitle>
+        <ListWrapper>
+          <List>
+            {data.importance.items.map((item, idx) => (
+              <ItemList key={idx}>
+                <ItemListTitle>{item.title}</ItemListTitle>
+                <ItemListDescription>{item.description}</ItemListDescription>
+              </ItemList>
+            ))}
+          </List>
+        </ListWrapper>
+      </BasicBoxWrapper>
+
+      <BasicBoxWrapper>
+        <Conclusion>{data.conclusion}</Conclusion>
+      </BasicBoxWrapper>
+    </>
   );
 };
 
